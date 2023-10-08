9fa11aa951b4dd8a24fb44615fcf812aab2256e2
Xana Green <byuixana@byui.edu>
2023-10-07 23:50:49 -0600

    Meta tags and Last Modified

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -5,3 +5,10 @@ hamButton.addEventListener('click', () => {
    navigation.classList.toggle('navigation');
    hamButton.classList.toggle('open');
 })
+
+// Mod date
+let lastModified = document.lastModified;
+let lastModifiedElement = document.querySelector("#mod");
+if (lastModified) {
+    lastModifiedElement.innerHTML = `Last Modified: ${lastModified}`
+}
