00893c57f5155296ea066f51203e359d10d59325
Xana Green <byuixana@byui.edu>
2023-10-12 09:14:05 -0600

    div name fixed

--- week03/scripts/lazy.js
+++ week03/scripts/lazy.js
@@ -2,6 +2,6 @@ let lastModified = document.lastModified;
 let lastModifiedDiv = document.querySelector("#last-modified");
 
 if (lastModified) {
-    lastModifiedElement.innerHTML = `Last Modified: ${lastModified}`
+    lastModifiedDiv.innerHTML = `Last Modified: ${lastModified}`
 };
 
