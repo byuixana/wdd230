f114a7b9ee53167bd850ec483a8f9dc6f2646076
Xana Green <byuixana@byui.edu>
2023-10-21 17:03:51 -0600

    Image effects and Javascript

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -12,3 +12,36 @@ let lastModifiedElement = document.querySelector("#mod");
 if (lastModified) {
     lastModifiedElement.innerHTML = `Last Modified: ${lastModified}`
 }
+
+// Visit # 
+
+// const visitsDisplay = document.querySelector("#visits");
+
+
+
+
+// if (userVisitNum == 0)
+// {
+//     console.log("Hello!")
+//     
+// }
+
+const visitsDisplay = document.querySelector("#visits");
+let userVisitNum = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+let visitDays = Number(window.localStorage.getItem("timeBetween"));
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+let daysSinceVisit = visitDays - Date.now();
+
+if (numVisits == 0) {
+    visitsDisplay.textContent = "Welcome! Let us know if you have any questions."
+} else if (daysSinceVisit < 0)
+{
+    visitsDisplay.textContent = "Back so soon! Awesome!"
+}else if (daysSinceVisit > 0)
+{
+    visitsDisplay = `You last visited ${daysBetween} days ago.`
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
