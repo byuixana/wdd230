32c41faf93c25815375d70ebde15b267da85d5cc
Xana Green <byuixana@byui.edu>
2023-12-09 22:39:15 -0700

    Fixed submission

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -3,7 +3,7 @@ const submitBtn = document.querySelector("#submitJoinBtn");
 const timestamp = document.querySelector("#timestamp");
 let submissionDate = new Date().toString();
 
-submitBtn.addEventListener('onclick', submissionDate);
+submitBtn.addEventListener('click', submissionDate);
 
 function setTime(){
     timestamp.innerHTML = submissionDate;
