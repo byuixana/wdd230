2c252917bde0f2577c7aae3d8a7997d902bbfb5b
Xana Green <byuixana@byui.edu>
2023-12-09 22:31:43 -0700

    Fixed chamber

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -15,16 +15,7 @@ if (lastModified) {
 
 
 
-// Timestamp
-const submitBtn = document.querySelector("#submitJoinBtn");
-const timestamp = document.querySelector("#timestamp");
-let submissionDate = new Date().toString();
 
-submitBtn.addEventListener('onclick', submissionDate);
-
-function setTime(){
-    timestamp.innerHTML = submissionDate;
-}
 
 
 
